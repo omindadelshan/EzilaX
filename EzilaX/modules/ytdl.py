c80cc41ba3660fe4326c799000485b4c74946235
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 06:45:33 -0700

    Update ytdl.py

--- EzilaX/modules/ytdl.py
+++ EzilaX/modules/ytdl.py
@@ -14,7 +14,7 @@ from youtube_dl.utils import (
     XAttrMetadataError,
 )
 
-from EzilaXBotV.events import register as saitama
+from EzilaX.events import register as saitama
 
 
 @saitama(pattern="^/yt(audio|video) (.*)")
