8562b42c52c800dbac23f1e11fcc886b2b0eca5d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 05:38:04 -0700

    Update forceSubscribe_sql.py

--- EzilaX/modules/sql/forceSubscribe_sql.py
+++ EzilaX/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 #    Copyright (C) 2020-2021 by @SadewJ
-#    This program is a part of DaisyX TG bot project
+#    This program is a part of EzilaX TG bot project
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -16,7 +16,7 @@
 
 from sqlalchemy import Column, Numeric, String
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from EzilaX.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
