0d2efa6bc2d2d5625a9672fba21f0dd7ee10f260
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 06:17:55 -0700

    Update narutomusic.py

--- EzilaX/modules/narutomusic.py
+++ EzilaX/modules/narutomusic.py
@@ -17,8 +17,8 @@ from youtube_dl.utils import (
     XAttrMetadataError,
 )
 
-from EzilaXBotV.events import register
-from EzilaXBotV.utils import progress
+from EzilaX.events import register
+from EzilaX.utils import progress
 
 try:
 
