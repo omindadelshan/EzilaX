de38d7f80c6dc069a008f8808e7ab4b008ac8776
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 05:27:28 -0700

    Update extraction.py

--- EzilaX/modules/helper_funcs/extraction.py
+++ EzilaX/modules/helper_funcs/extraction.py
@@ -5,8 +5,8 @@ from telegram.error import BadRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
-from EzilaXBotV import LOGGER
-from EzilaXBotV.modules.users import get_user_id
+from EzilaX import LOGGER
+from EzilaX.modules.users import get_user_id
 
 
 def id_from_reply(message):
