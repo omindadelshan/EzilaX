7ae5a234357d8d17643e0828d4a18108baf1533a
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 06:19:54 -0700

    Update phone.py

--- EzilaX/modules/phone.py
+++ EzilaX/modules/phone.py
@@ -3,9 +3,9 @@ import json
 import requests
 from telegram.ext import CommandHandler, run_async
 
-from EzilaXBotV import dispatcher
-from EzilaXBotV.modules.helper_funcs.alternate import send_message
-from EzilaXBotV.modules.helper_funcs.chat_status import user_admin
+from EzilaX import dispatcher
+from EzilaX.modules.helper_funcs.alternate import send_message
+from EzilaX.modules.helper_funcs.chat_status import user_admin
 
 
 @run_async
