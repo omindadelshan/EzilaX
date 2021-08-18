f41a1db0b69f9848eb3cc8396d9b7047a36475de
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 05:34:02 -0700

    Update antiflood_sql.py

--- EzilaX/modules/sql/antiflood_sql.py
+++ EzilaX/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, String, UnicodeText
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from EzilaX.modules.sql import BASE, SESSION
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
