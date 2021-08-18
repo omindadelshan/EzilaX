34272328497f071762bdbab137284e2fac6d5639
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 05:52:26 -0700

    Update connection.py

--- EzilaX/modules/connection.py
+++ EzilaX/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackQueryHandler, CommandHandler, run_async
 
-import EzilaXBotV.modules.sql.connection_sql as sql
-from EzilaXBotV import DEV_USERS, DRAGONS, dispatcher
-from EzilaXBotV.modules.helper_funcs import chat_status
-from EzilaXBotV.modules.helper_funcs.alternate import send_message, typing_action
+import EzilaX.modules.sql.connection_sql as sql
+from EzilaX import DEV_USERS, DRAGONS, dispatcher
+from EzilaX.modules.helper_funcs import chat_status
+from EzilaX.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
