f643d7e1400c065f9133555d4b94c5f4e5c245e5
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 06:02:44 -0700

    Update dev.py

--- EzilaX/modules/dev.py
+++ EzilaX/modules/dev.py
@@ -6,8 +6,8 @@ from time import sleep
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from EzilaXBotV import dispatcher
-from EzilaXBotV.modules.helper_funcs.chat_status import dev_plus
+from EzilaX import dispatcher
+from EzilaX.modules.helper_funcs.chat_status import dev_plus
 
 
 @run_async
