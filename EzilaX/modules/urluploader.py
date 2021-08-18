7e9ad423c676d567a5ccde1b4239de8365c77073
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 06:39:16 -0700

    Update urluploader.py

--- EzilaX/modules/urluploader.py
+++ EzilaX/modules/urluploader.py
@@ -3,7 +3,7 @@ import time
 
 import aiohttp
 
-from EzilaXBotV.utils.uputils import humanbytes, time_formatter
+from EzilaX.utils.uputils import humanbytes, time_formatter
 
 
 async def download_file(url, file_name, message, start_time, bot):
