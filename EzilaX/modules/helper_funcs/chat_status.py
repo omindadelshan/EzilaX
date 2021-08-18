ce5903e42e844cbc6930eeea35549064a1d2ed5d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-18 05:26:46 -0700

    Update chat_status.py

--- EzilaX/modules/helper_funcs/chat_status.py
+++ EzilaX/modules/helper_funcs/chat_status.py
@@ -5,7 +5,7 @@ from cachetools import TTLCache
 from telegram import Chat, ChatMember, ParseMode, Update
 from telegram.ext import CallbackContext
 
-from EzilaXBotV import (
+from EzilaX import (
     DEL_CMDS,
     DEMONS,
     DEV_USERS,
