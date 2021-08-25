08b55e4c6bd144f1cb145c8f1c6ca5bc3d54d7a6
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-25 19:07:10 +0530

    Update urluploader.py

--- EzilaX/modules/urluploader.py
+++ EzilaX/modules/urluploader.py
@@ -87,6 +87,6 @@ __help__ = """
 
 • /up : reply to a direct download link to upload it to telegram as files
  
-© @SadewJ 🇱🇰
+© @omindas 🇱🇰
 """
-__mod_name__ = "URL Upload"
+__mod_name__ = "URL Upload 🚀"
